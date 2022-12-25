74cf2ff29cfa3625c974d56af0be2979bf2e16dc
Ufuk AKKAYA <akkaya_ufuk@hotmail.com>
2022-12-25 15:39:00 +0300

    --> FCFS kuyruğundaki prosesler işlendi. --> Proses başlama yürütülme ve sonlanma printleri yazıldı. Thread oluşturma run etme sonra yapılacak.

--- src/Process.java
+++ src/Process.java
@@ -23,4 +23,34 @@ public class Process {
 		ProcessState = State.Create;
 	}
 	
+	void Run(int sysTime)
+	{		
+		if(ProcessState == State.Create)
+		{
+			// proses başladı
+			System.out.printf("%d sn proses başladı.          (id:%d öncelik:%d kalan süre:%d sn)\r\n", sysTime, 
+																									PID,
+																									Priority,
+																									BrustTime);
+			ProcessState = State.Run;
+		}
+		else if(BrustTime == 1)
+		{
+			// proses sonlandı 
+			System.out.printf("%d sn proses sonlandı.          (id:%d öncelik:%d kalan süre:%d sn)\r\n", sysTime, 
+																									 PID,
+																									 Priority,
+																									 BrustTime);
+			ProcessState = State.Terminated;
+		}
+		else
+		{			
+			// proses yürütülüyor
+			System.out.printf("%d sn proses yürütülüyor.          (id:%d öncelik:%d kalan süre:%d sn)\r\n", sysTime, 
+					 																					PID,
+					 																					Priority,
+					 																					BrustTime);
+		}
+		BrustTime--;
+	}
 }
--- src/main.java
+++ src/main.java
@@ -1,6 +1,7 @@
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.lang.Thread;
 
 public class main {
 
@@ -12,13 +13,14 @@ public class main {
 		Queue<Process> Queue_RQ1 = new LinkedList<Process>();
 		Queue<Process> Queue_RQ2 = new LinkedList<Process>();
 		
+			
 		// TODO: DOSYADAN OKUNACAK
 		
 		JobDispatchList.add(new Process(1, 0 , 0, 10));
-		JobDispatchList.add(new Process(2, 1 , 5, 6));
-		JobDispatchList.add(new Process(3, 2 , 2, 8));
-		JobDispatchList.add(new Process(4, 3 , 18, 12));
-		JobDispatchList.add(new Process(5, 1 , 6, 5));		
+		JobDispatchList.add(new Process(2, 0 , 5, 6));
+		JobDispatchList.add(new Process(3, 0 , 2, 8));
+		JobDispatchList.add(new Process(4, 0 , 18, 12));
+		JobDispatchList.add(new Process(5, 0 , 6, 5));		
 		
 		int tick = 0;
 		for(;;)
@@ -28,26 +30,67 @@ public class main {
 				for(int i =0 ; i<JobDispatchList.size(); i++ )
 				{
 					int arriveTime = JobDispatchList.get(i).ArriveTime;
-					if(arriveTime < tick)
+					if(arriveTime >= tick)
 					{
-						System.out.printf("%d PID Run ArriveTime:%d tick=%d\n", JobDispatchList.get(i).PID, arriveTime, tick);
+						//System.out.printf("%d PID Run ArriveTime:%d tick=%d\n", JobDispatchList.get(i).PID, arriveTime, tick);
+						
+						Process process = JobDispatchList.get(i);						
+						
+						// Proses ilgili kuyruğa eklenir.
+						switch(process.Priority)
+						{
+							case 0: Queue_FCFS.add(process);
+									break;
+							case 1: Queue_RQ0.add(process);
+									break;
+							case 2: Queue_RQ1.add(process);
+									break;
+							case 3: Queue_RQ2.add(process);
+									break;
+						}
+						
+						// Başlatılacak görev listesinden kaldırır.
 						JobDispatchList.remove(i);
 						break;
 					}
 				}
 			}
-			else
+			else if(Queue_FCFS.isEmpty() &&
+					Queue_RQ0.isEmpty() && 
+					Queue_RQ1.isEmpty() &&
+					Queue_RQ2.isEmpty())
+			{
+				
+				System.out.printf("Program Sonlandi.");
 				break;
+			}
 			
-			
-			//Thread.sleep(1000);	
+			// FCFS Yüksek öncelikli bir proses varsa
+			if(!Queue_FCFS.isEmpty())
+			{
+				Process runProcess = Queue_FCFS.peek();				
+				
+				// Proses Start edilir.				
+				runProcess.Run(tick);		
+				
+				// Proses bittiyse listeden çıkarılır.
+				if(runProcess.ProcessState == Process.State.Terminated)
+				{				
+					Queue_FCFS.remove();			
+				}
+			}
+				
+			try
+			{
+				Thread.sleep((long)1);
+			}
+			catch (Exception e) {
+		           
+	            // catching the exception
+	            System.out.println(e);
+	        }		
 			tick++;
 		}
-		
-		
-		//myList.PID
-		System.out.printf("JAVA PROJESI Bitti");
-
 	}
 
 }
